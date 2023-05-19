29c2499e11238a8e33b915c36c1d1dcb75966515
Pedro Monteiro <pedromont436@gmail.com>
2023-05-19 10:40:07 -0300

    Payment entity

--- src/main/java/com/mont/dscommerce/model/Payment.java
+++ src/main/java/com/mont/dscommerce/model/Payment.java
@@ -2,16 +2,13 @@ package com.mont.dscommerce.model;
 
 import java.time.Instant;
 
-import org.hibernate.annotations.ManyToAny;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,23 +17,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_order")
+@Table(name = "tb_payment")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Order {
+public class Payment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
-     
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    
+    @OneToOne
+    @MapsId
+    private Order order;
 }
